1c8d172396d673a9f12f39f33ec87580f3545077
Nathan Batchelder <natebatchelder@gmail.com>
2021-07-30 10:45:37 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const input = "hello world";
+const whaleVowels = ['a', 'e', 'i', 'o', 'u'];
+let resultArray = [];
+
+for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
+    console.log("inputIndex = " = input[inputIndex]);
+
+
+}
+
+
+
+et i = 0; i < input.length; i++) {
+    let input = 'hello world'
+    console.log(array[i]);
+}
+
