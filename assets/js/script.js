eb0d6fbc44a102a51580bf1447a65b2123e6bd3c
Nathan Batchelder <natebatchelder@gmail.com>
2021-07-30 11:30:57 -0700

    js for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,26 @@
 const input = "hello world";
-const whaleVowels = ['a', 'e', 'i', 'o', 'u'];
+const vowel = ['a', 'e', 'i', 'o', 'u'];
 let resultArray = [];
 
 for (let inputIndex = 0; inputIndex < input.length; inputIndex++) {
-    console.log("inputIndex = " = input[inputIndex]);
+    // console.log("inputIndex = " = input[inputIndex]);
+    for (let vowel = 0; vowel < vowel.length; vowel++) {
+        // console.log("vowel =" = vowel);
+        if (input[inputIndex] === vowels[vowel]) {
+            if (input[inputIndex] === 'e') {
+                resultArray.push('ee')
+            } else if
+                (input[inputIndex] === 'u') {
+                resultArray.push('uu')
+            }
+        }
 
-
+        else {
+            resultArray.push(input[inputIndex]);
+        }
+    }
 }
 
+console.log(resultArray.join("").toUpperCase());
 
-
-et i = 0; i < input.length; i++) {
-    let input = 'hello world'
-    console.log(array[i]);
-}
 
